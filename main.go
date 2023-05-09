2f1be6a3b5283652739581467423decc08a2ad89
bennethfreeman <bennethfreeman@gmail.com>
2023-05-09 16:06:47 -0700

    cryptit : changes for v0.1.0

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Priyanka-yadavv/cryptit/decrypt"
+	"github.com/Priyanka-yadavv/cryptit/encrypt"
+)
+
+func main() {
+	encryptedStr := encrypt.Nimbus("Benneth")
+	fmt.Println(encryptedStr)
+
+	fmt.Println(decrypt.Nimbus(encryptedStr))
+}
